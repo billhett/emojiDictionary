55913ff724d3d3ef0690ae1abf2a5b926970892b
William Hettinger <bill.hettinger@gmail.com>
2018-02-24 17:08:20 -0500

    add definition to dictionary controller for each emoji

--- emojiDictionary/DefinitionViewController.swift
+++ emojiDictionary/DefinitionViewController.swift
@@ -14,19 +14,40 @@ class DefinitionViewController: UIViewController {
     
     @IBOutlet weak var bigEmojiLabel: UILabel!
     
+    @IBOutlet weak var definitionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         print("in 2nd vc: \(emoji)")
         bigEmojiLabel.text = emoji
+        if emoji == "😍" {
+            definitionLabel.text = "Heart eyes"
+        } else if emoji == "😎"{
+            definitionLabel.text = "Sunglass dude"
+        } else if emoji == "🤠"{
+            definitionLabel.text = "Cowboy dude"
+        }  else if emoji == "😫"{
+            definitionLabel.text = "Sad dude"
+        } else if emoji == "🐹"{
+            definitionLabel.text = "Cute Hampster"
+        } else if emoji == "🍼"{
+            definitionLabel.text = "Baby Bottle"
+        } else if emoji == "🍸"{
+            definitionLabel.text = "Martini"
+        } else if emoji == "👽"{
+            definitionLabel.text = "Alien"
+        } else {
+            definitionLabel.text = "no emoji"
+        
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+}
+
 
 
 }
