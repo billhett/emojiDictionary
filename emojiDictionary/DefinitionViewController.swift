4dbe9e9453126c14b2e100a07ba76cebc0acce50
William Hettinger <bill.hettinger@gmail.com>
2018-02-24 10:48:57 -0500

    Connect viewControllers with segue

---
+++ emojiDictionary/DefinitionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DefinitionViewController.swift
+//  emojiDictionary
+//
+//  Created by William Hettinger on 2/24/18.
+//  Copyright © 2018 William Hettinger. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
