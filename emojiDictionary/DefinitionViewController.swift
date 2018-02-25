5a9c610155c3bd5fe67558c83e54ea39a63ade12
William Hettinger <bill.hettinger@gmail.com>
2018-02-25 10:36:18 -0500

    Add Category and Date to emojis in if statements

--- emojiDictionary/DefinitionViewController.swift
+++ emojiDictionary/DefinitionViewController.swift
@@ -16,6 +16,12 @@ class DefinitionViewController: UIViewController {
     
     @IBOutlet weak var definitionLabel: UILabel!
     
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    @IBOutlet weak var birthYearLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,31 +29,47 @@ class DefinitionViewController: UIViewController {
         bigEmojiLabel.text = emoji
         if emoji == "😍" {
             definitionLabel.text = "Heart eyes"
+            categoryLabel.text = "Category: Smiley"
+            birthYearLabel.text = "BirthYear: 2009"
         } else if emoji == "😎"{
             definitionLabel.text = "Sunglass dude"
+            categoryLabel.text = "Category: Smiley"
+            birthYearLabel.text = "BirthYear: 2008"
         } else if emoji == "🤠"{
             definitionLabel.text = "Cowboy dude"
+            categoryLabel.text = "Category: Smiley"
+            birthYearLabel.text = "BirthYear: 2002"
         }  else if emoji == "😫"{
             definitionLabel.text = "Sad dude"
+            categoryLabel.text = "Category: Smiley"
+            birthYearLabel.text = "BirthYear: 2001"
         } else if emoji == "🐹"{
             definitionLabel.text = "Cute Hampster"
+            categoryLabel.text = "Category: Animal"
+            birthYearLabel.text = "BirthYear: 2011"
         } else if emoji == "🍼"{
             definitionLabel.text = "Baby Bottle"
+            categoryLabel.text = "Category: Thing"
+            birthYearLabel.text = "BirthYear: 2009"
         } else if emoji == "🍸"{
             definitionLabel.text = "Martini"
+            categoryLabel.text = "Category: Thing"
+            birthYearLabel.text = "BirthYear: 2007"
         } else if emoji == "👽"{
             definitionLabel.text = "Alien"
+            categoryLabel.text = "Category: Smiley"
+            birthYearLabel.text = "BirthYear: 2009"
         } else {
             definitionLabel.text = "no emoji"
-        
+            
+        }
     }
-}
-
-override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-}
-
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
 }
